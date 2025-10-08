2dc73a9d63ca831807417a36b03d7766d4eafacd
Andyish-tech <ishmukazi@gmail.com>
2025-10-08 10:24:34 +0200

    foreach and For_In_Loop

---
+++ loops/for_In_Loop.dart
@@ -0,0 +1,8 @@
+void main(){
+  
+  List<String> fruits=['Apple', 'Banana', 'Orange', 'Grape'];
+  print('value any index: {$fruits[0]}');
+  for(var fruit in fruits){
+    print(fruit);
+  }
+}
---
+++ loops/for_each.dart
@@ -0,0 +1,17 @@
+void main(){
+  //For each loop.
+  /*
+  collectionName.forEach((element){
+  code to be executed for each elemennt.
+  });
+  ex: write dart program to print each element of a list using for each loop.
+
+  List<int>: Data type
+  List<dynamic>: dynamic data type
+  */ 
+  List<int> numbers=[10,20,30,40,50];
+  numbers.forEach((element) {
+    print(element);
+  });
+
+}
