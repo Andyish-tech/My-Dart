8b607b49433d04f94758177e3dd588e8b684b3bc
Andyish-tech <ishmukazi@gmail.com>
2025-10-08 11:04:34 +0200

    built_in function

---
+++ functions/built_In.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+void main(){
+  int num=100;
+  double result=sqrt(num);
+  print('Square root of $num is $result');
+  print ('PI constant value is:${pi}');
+  print('e constant value is: ${e}');
+  print('Random number between 0 and 100: ${
+    Random().nextInt(100)}');
+}
