5b650ea5fac24eb9e6f0766bcd69d64f3ca14ba8
Andyish-tech <ishmukazi@gmail.com>
2025-10-29 10:26:30 +0200

    Class Cunstructor is created

---
+++ oop/car.dart
@@ -0,0 +1,16 @@
+class Car{
+  //Properties
+  String ? carName;
+  String ? brand;
+  int ? year;
+  String ? color;
+  //Constructor
+  Car(){
+    print('Constructor is Created');
+  }
+
+  void drive(){
+    print('$carName is Driving');
+  }
+
+}
--- oop/main.dart
+++ oop/main.dart
@@ -1,8 +1,13 @@
-import 'person.dart';
+// import 'person.dart';
+// void main(){
+//   Person p2=Person();
+//   p2.name='Peter';
+//   p2.age=26;
+//   p2.email='peter@gmail.com';
+//   p2.displayInfo();
+// }
+import 'car.dart';
 void main(){
-  Person p2=Person();
-  p2.name='Peter';
-  p2.age=26;
-  p2.email='peter@gmail.com';
-  p2.displayInfo();
+  Car myCar=Car();
+
 }
