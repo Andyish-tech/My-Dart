c4b81aad9d64d59ccd99e441d08fa25b143272ef
Andyish-tech <ishmukazi@gmail.com>
2025-11-10 11:55:56 +0200

    Polymoriphism

--- oop/Polymoriphism/shape.dart
+++ oop/Polymoriphism/shape.dart
@@ -8,5 +8,24 @@ class Shape {
 }
 
 class Rectangle extends Shape{
-  
+  Rectangle(double length, double height):super(length,height);
+  @override
+  double area(){
+    return length!*height!;
+  }
+}
+
+class Triangle extends Shape{
+  Triangle(double length, double height):super(length,height);
+  @override
+  double area(){
+    return 0.5*length!*height!;
+  }
+}
+
+void main(){
+  var rectangle=Rectangle(5.0, 10.0);
+  print("Area of Rectangle: ${rectangle.area()}");
+  var triangle=Triangle(6.0, 15.0);
+  print("Area of Triangle: ${triangle.area()}");
 }
